52e2a4ecfd7c38c332efc21b13b4d5d191595f50
loraii-Prod <loraii.gaming@gmail.com>
2026-09-06 21:39:01 +0200

    Add Relay UI components

---
+++ src/components/brand.tsx
@@ -0,0 +1,4 @@
+import Link from "next/link";
+export function Brand() {
+  return <Link href="/" className="brand" aria-label="Relay home"><span className="mark">R</span><span>RELAY</span><small>REMOTE PRODUCTION</small></Link>;
+}
